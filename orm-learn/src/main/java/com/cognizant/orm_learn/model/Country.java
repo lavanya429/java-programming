290875fafa83ffada757b4dae2354bf2cf7d3621
lavanya429 <manilavanya2004@gmail.com>
2025-07-11 10:39:02 +0530

    Add files via upload

---
+++ orm-learn/src/main/java/com/cognizant/orm_learn/model/Country.java
@@ -0,0 +1,36 @@
+package com.cognizant.orm_learn.model;
+
+import jakarta.persistence.*;
+
+
+@Entity
+public class Country {
+
+    @Id
+    private String code;
+
+    private String name;
+
+    // Getters and Setters
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Country [code=" + code + ", name=" + name + "]";
+    }
+}
